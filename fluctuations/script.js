09ed2dffd0be8fb1be0ed01ab79027bb12a36ff3
DebDoDab <vadim.klishevich@mail.ru>
2018-05-16 09:33:24 +0300

    wow omg fluctuations r running

--- fluctuations/script.js
+++ fluctuations/script.js
@@ -6,6 +6,7 @@ var
 	canv.height = window.innerHeight;
 
 var
+	color = 0,
 	w = 3,
 	a = 70,
 	r = 100,
@@ -14,12 +15,13 @@ var
 	t = 10;				//framerate
 
 var
-	object = [{x: 0, y: 0, r: r, w: w, A: a, start: start, color: "#f00"},
-			  {x: 0, y: 0, r: r*0.8, w: w*0.8, A: a*0.8, start: start, color: "#0f0"}
-	];
+	object = [];
 
 var
-	O = [[],[]];
+	O = [];
+
+AddPendulum();
+AddPendulum();
 
 setInterval(function () {
 
@@ -38,7 +40,14 @@ setInterval(function () {
 		O[i].unshift({x: object[i].x, y: 0-r/2});
 		for (var j = 0; j < O[i].length; j++) {
 			O[i][j].y -= t/20;
+
+			if (O[i][j].y < -canv.height - 2*r-canv.height/50) {
+				O[i].splice(j, 1);
+				j--;
+			}
 		}
+
+
 	}
 
 	ctx.translate(canv.width/2, canv.height - 2*r);
@@ -83,3 +92,49 @@ setInterval(function () {
 	ctx.translate(-canv.width/2, -canv.height + 2*r);
 
 }, t);
+
+
+function getRandomArbitrary(min, max) {
+ 	return Math.random() * (max - min) + min;
+}
+
+function rgbToHex(r, g, b) {
+	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function getColor(color)
+{
+	color %= 768;
+	if (color < 256)
+	{
+		return rgbToHex(255 - color, color, 0);
+	}
+	if (color < 512)
+	{
+		return rgbToHex(0, 255 - (color - 256), color - 256);
+	}
+	if (color < 768)
+	{
+		return rgbToHex(color - 512, 0, 255 - (color - 512));
+	}
+}
+
+function getNextColor()
+{
+	color += 160;
+	color %= 768;
+	return getColor(color);
+}
+
+function AddPendulum() {
+	O.push([]);
+	var kola = getRandomArbitrary(0.5, 1.5);
+	var col = getNextColor();
+	object.push(
+	{x: 0, y: 0, r: r*kola, w: w*kola, A: a*kola, start: start, color: col});
+}
+
+function DeletePendulum() {
+	O.splice(O.length-1, 1);
+	object.splice(object.length-1, 1);
+}
