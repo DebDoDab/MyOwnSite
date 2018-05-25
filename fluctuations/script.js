38df07f86f9c113dae53a661d4ed9aec5a74293c
DebDoDab <vadim.klishevich@mail.ru>
2018-05-25 13:48:10 +0300

    ЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭЭ

--- fluctuations/script.js
+++ fluctuations/script.js
@@ -6,7 +6,7 @@ var
 	canv.height = window.innerHeight;
 
 var
-	color = 0,
+	speed = 0.1,
 	w = 3,
 	a = 70,
 	r = 100,
@@ -15,127 +15,152 @@ var
 	t = 10;				//framerate
 
 var
-	object = [];
+	object = {
+		r : 10,
+		a : 80,
+		g : 0.003,
+		w : 0,
+		l : 100,
+		f : 0,
+		x : 0,
+		y : 0,
+		angle : 0
+	},
 
-var
-	O = [];
+	camera = {
+		x : canv.width/7,
+		y : canv.height - 2*r
+	},
 
-AddPendulum();
-AddPendulum();
+	track = [],
+
+	chart_xt = {
+		x : canv.width*3/4,
+		y : canv.height/3,
+		track : []
+	}
 
 setInterval(function () {
 
+	//update
 	canv.width  = window.innerWidth;
 	canv.height = window.innerHeight;
+
+	camera.x = canv.width/7;
+	camera.y = canv.height - 2*r;
+
+	chart_xt.x = canv.width*3/4;
+	chart_xt.y = canv.height/4;
+
+	//object.w = object.g //fix plz
+	object.w = document.getElementById("pendulumw").value/100;
+	object.a = document.getElementById("penduluma").value;
+	object.x = object.a * Math.sin(object.w*t + object.angle);
+	object.angle += object.w*t;
+	object.y = Math.sqrt(object.l*object.l - object.x*object.x);
+
+	track.push({
+		x : object.x,
+		y : 0 - r/2
+	});
+	chart_xt.track.push(object.x)
+
+	for (var i = 0; i < track.length; i++) {
+		track[i].y -= t*speed;
+	}
+
+	while(track[0].y < -camera.y) {
+		track.splice(0, 1);
+	}
+
+	console.log(chart_xt.track[chart_xt.track.length -1], chart_xt.track[0]);
+	while(chart_xt.track.length > 300) {
+		chart_xt.track.splice(0, 1);
+	}
 
 	//clear screen
 	ctx.fillStyle = "#fff";
 	ctx.fillRect(0, 0, canv.width, canv.height);
 
-	time += t;
-	for (var i = 0; i < object.length; i++) {
-		object[i].x = object[i].A * Math.sin(object[i].w*time/1000+object[i].start);
-		object[i].y = Math.sqrt(object[i].r*object[i].r - object[i].x*object[i].x);
+	//draw pendulum
+	ctx.translate(camera.x, camera.y);
 
-		O[i].unshift({x: object[i].x, y: 0-r/2});
-		for (var j = 0; j < O[i].length; j++) {
-			O[i][j].y -= t/20;
+	//draw center of pendulum
+	ctx.fillStyle = "#000";
+	ctx.beginPath();
+	ctx.arc(0,0, 15, 0, Math.PI*2);
+	ctx.fill();
 
-			if (O[i][j].y < -canv.height - 2*r-canv.height/50) {
-				O[i].splice(j, 1);
-				j--;
-			}
-		}
-
-
+	//draw track
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = "#00f";
+	ctx.beginPath();
+	ctx.moveTo(object.x, object.y);
+	ctx.lineTo(object.x, 0 - r/2);
+	for (var i = track.length-1; i >=0; i--) {
+		ctx.lineTo(track[i].x, track[i].y);
 	}
+	ctx.stroke();
 
-	ctx.translate(canv.width/2, canv.height - 2*r);
+	//draw line upper pendulum
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "#0f0";
+	ctx.beginPath();
+	ctx.moveTo(0 - r, 0 - r/2);
+	ctx.lineTo(0 + r, 0 - r/2);
+	ctx.stroke();
 
-		for (var i = 0; i < object.length; i++) {
+	//draw line from pendulum to the center
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = "#000";
+	ctx.beginPath();
+	ctx.moveTo(object.x, object.y);
+	ctx.lineTo(0, 0);
+	ctx.stroke();
 
-			ctx.strokeStyle = object[i].color;
-			ctx.lineWidth = 1;
-			ctx.beginPath();
-			ctx.moveTo(object[i].x, object[i].y);
-			ctx.lineTo(0, 0);
-			ctx.stroke();
+	//draw pendulum
+	ctx.fillStyle = "#f00";
+	ctx.beginPath();
+	ctx.arc(object.x, object.y, object.r, 0, Math.PI*2);
+	ctx.fill();
 
-			ctx.fillStyle = object[i].color;
-			ctx.beginPath();
-			ctx.arc(object[i].x,object[i].y, 10, 0, Math.PI*2);
-			ctx.fill();
+	ctx.translate(-camera.x, -camera.y);
 
-			ctx.lineWidth = 3;
-			ctx.strokeStyle = object[i].color;
-			ctx.beginPath();
-			ctx.moveTo(object[i].x, object[i].y);
-			for (var j = 0; j < O[i].length; j++) {
-				ctx.lineTo(O[i][j].x, O[i][j].y);
-			}
-			ctx.stroke();
+	//chart x(t)
+	ctx.translate(chart_xt.x, chart_xt.y);
 
-		}
+	//axis X
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = "#000";
+	ctx.beginPath();
+	ctx.moveTo(0, 0);
+	ctx.lineTo(303, 0);
+	ctx.moveTo(305, 0);
+	ctx.lineTo(290, -7);
+	ctx.moveTo(305, 0);
+	ctx.lineTo(290, 7);
+	ctx.moveTo(0, -153);
+	ctx.lineTo(0, +153);
+	ctx.moveTo(0, -155);
+	ctx.lineTo(-7, -145);
+	ctx.moveTo(0, -155);
+	ctx.lineTo(7, -145);
+	ctx.stroke();
 
-		ctx.fillStyle = "#000";
-		ctx.beginPath();
-		ctx.arc(0,0, 15, 0, Math.PI*2);
-		ctx.fill();
+	ctx.strokeStyle = "#0f0";
+	ctx.beginPath();
+	ctx.moveTo(0, chart_xt.track[0]);
+	for (var i = 1; i < chart_xt.track.length; i++) {
+		ctx.lineTo(i, chart_xt.track[i]);
+	}
+	ctx.stroke();
 
-		ctx.lineWidth = 5;
-		ctx.strokeStyle = "#0f0";
-		ctx.beginPath();
-		ctx.moveTo(0 - r, 0 - r/2);
-		ctx.lineTo(0 + r, 0 - r/2);
-		ctx.stroke();
 
-	ctx.translate(-canv.width/2, -canv.height + 2*r);
+	ctx.translate(chart_xt.x, chart_xt.y);
 
 }, t);
 
 
 function getRandomArbitrary(min, max) {
  	return Math.random() * (max - min) + min;
-}
-
-function rgbToHex(r, g, b) {
-	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
-
-function getColor(color)
-{
-	color %= 768;
-	if (color < 256)
-	{
-		return rgbToHex(255 - color, color, 0);
-	}
-	if (color < 512)
-	{
-		return rgbToHex(0, 255 - (color - 256), color - 256);
-	}
-	if (color < 768)
-	{
-		return rgbToHex(color - 512, 0, 255 - (color - 512));
-	}
-}
-
-function getNextColor()
-{
-	color += 160;
-	color %= 768;
-	return getColor(color);
-}
-
-function AddPendulum() {
-	O.push([]);
-	var kola = getRandomArbitrary(0.5, 1.5);
-	var loka = getRandomArbitrary(0.5, 1.5);
-	var col = getNextColor();
-	object.push(
-	{x: 0, y: 0, r: r*kola, w: w*loka, A: a*kola, start: start, color: col});
-}
-
-function DeletePendulum() {
-	O.splice(O.length-1, 1);
-	object.splice(object.length-1, 1);
 }
